bf4b55673a1aaee80add9c5ce7c27bb61411792c
kurniawanedi <edikurniawan27@outlook.com>
2021-09-04 15:04:26 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>Personal Website</p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    GitHub
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default App;
