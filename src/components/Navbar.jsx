dc401fb97d1ee45c2e2d8f315b856ab1da73d0c4
kurniawanedi <edikurniawan27@outlook.com>
2021-09-05 19:06:48 +0700

    add footer and update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Logo from '../images/logo.png';
 
 const Navbar = () => {
     return (
-        <div>
-            <ul>
+        <div className="flex justify-between items-center py-5 bg-blue-500">
+            <div className="">
+                <NavLink exact to="/">
+                    <img src={Logo} className="h-14" alt="" srcset="" />
+                </NavLink>
+            </div>
+            <ul className="flex space-x-5">
                 <li>
                     <NavLink exact to="/">
                         Home
