74e60b1176402ddd55b7fac271a2c59b928739fb
kurniawanedi <edikurniawan27@outlook.com>
2021-09-06 20:15:58 +0700

    udate home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,21 @@ import Myfoto from '../images/me.png';
 const Home = () => {
     return (
         <div className="flex items-center justify-between">
-            <div className="w-3/5">
-                <h3>Hi, my name is Edi Kurniawan</h3>
-                <h1>I'm a freelance Web Developer</h1>
+            <div className="w-3/5 space-y-4">
+                <h3 className="font-poppins text-4xl">
+                    Hi, my name is Edi Kurniawan
+                </h3>
+                <h1 className="font-poppins font-bold text-4xl">
+                    I'm a freelance Web Developer
+                </h1>
+                <div>
+                    <a href="https://github.com" className="mr-4 text-red-500">
+                        <i className="fab fa-linkedin fa-3x"></i>
+                    </a>
+                    <a href="https://github.com" className="text-red-500">
+                        <i className="fab fa-github-square fa-3x"></i>
+                    </a>
+                </div>
             </div>
             <div className="w-2/5 pt-40 relative">
                 <div className="flex justify-end pr-5">
@@ -16,7 +28,7 @@ const Home = () => {
                         alt=""
                     />
                 </div>
-                <div className="bg-blue-500 ml-32 pb-80 rounded-3xl"></div>
+                <div className="bg-gradient-to-b from-green-100 to-blue-500 ml-32 pb-80 rounded-3xl"></div>
             </div>
         </div>
     );
