fd035f537747fb6f1e61c379336472e0b30cd160
kurniawanedi <edikurniawan27@outlook.com>
2021-09-06 20:17:30 +0700

    add font poppins and hind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            fontFamily: {
+                poppins: "'Poppins', 'sans-serif'",
+                hind: "'Hind', 'sans-serif'",
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
