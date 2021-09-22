97a1542fae3877f5fc7ca5bc9e5be24358870ae3
kurniawanedi <edikurniawan27@outlook.com>
2021-09-22 17:50:53 +0700

    update favicon etc

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import Router from './router/Index';
 import React from 'react';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';
 
 import './tailwind.css';
 
 const App = () => {
     return (
         <div className="flex flex-col h-screen">
-            <div className="container mx-auto max-w-6xl">
-                <Navbar />
+            <div className="">
+                <div className="container mx-auto max-w-6xl">
+                    <Navbar />
+                </div>
             </div>
             <div className="mb-auto container mx-auto max-w-6xl">
                 <Router />
-            </div>
-            <div className="w-full bg-gray-50">
-                <Footer />
             </div>
         </div>
     );
--- src/router/Index.jsx
+++ src/router/Index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from '../pages/Home';
-import Contact from '../pages/Contact';
 import Portfolio from '../pages/Portfolio';
 
 const Router = () => {
@@ -9,9 +8,6 @@ const Router = () => {
         <Switch>
             <Route exact path="/">
                 <Home />
-            </Route>
-            <Route exact path="/contact">
-                <Contact />
             </Route>
             <Route exact path="/portfolio">
                 <Portfolio />
