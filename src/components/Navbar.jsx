4eafe5daf5daf6ee2dc4854b9fe44c2394e2ed7d
kurniawanedi <edikurniawan27@outlook.com>
2021-09-24 21:47:26 +0700

    update navbar font and make it fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,52 +14,51 @@ const Navbar = () => {
         setColorBgIcon(!colorBgIcon);
     };
     return (
-        <div className="flex justify-between items-center py-7">
-            <div className="">
+        <header className="sticky top-0 z-50 bg-white">
+            <nav className="flex justify-between items-center text-lg py-4">
                 <NavLink exact to="/">
-                    <img src={Logo} alt="" className="h-14" />
+                    <img src={Logo} alt="" className="h-12" />
                 </NavLink>
-            </div>
-            <ul className="flex items-center space-x-10 font-poppins text-lg tracking-tight text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 to-blue-light">
-                <li>
-                    <NavLink to="/about-me">About Me</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/portfolio">Skill</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/portfolio">Experience</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/portfolio">Portfolio</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/portfolio">Resume</NavLink>
-                </li>
-
-                <li>
-                    <button
-                        className={
-                            colorBgIcon
-                                ? 'bg-gray-700 rounded-full'
-                                : 'bg-blue-light rounded-full'
-                        }
-                        onClick={handleClick}>
-                        <div className=" px-3 py-2 flex items-center justify-center text-white space-x-2">
-                            <i
-                                className={
-                                    icon
-                                        ? 'fas fa-moon fa-lg'
-                                        : 'fas fa-sun fa-lg'
-                                }></i>
-                            <p className="font-poppins text-lg tracking-tight">
-                                {iconWordmark ? 'Dark' : 'Light'}
-                            </p>
-                        </div>
-                    </button>
-                </li>
-            </ul>
-        </div>
+                <ul className="flex items-center space-x-10 font-hind text-blue-600">
+                    <li>
+                        <NavLink to="/about-me">About Me</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/skill">Skill</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/experience">Experience</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/portfolio">Portfolio</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/resume">Resume</NavLink>
+                    </li>
+                    <li>
+                        <button
+                            className={
+                                colorBgIcon
+                                    ? 'bg-gray-700 rounded-full'
+                                    : 'bg-blue-light rounded-full'
+                            }
+                            onClick={handleClick}>
+                            <div className=" py-2 px-3 flex items-center justify-center text-white space-x-2">
+                                <i
+                                    className={
+                                        icon
+                                            ? 'fas fa-moon fa-lg'
+                                            : 'fas fa-sun fa-lg'
+                                    }></i>
+                                <p className="font-hind font-semibold">
+                                    {iconWordmark ? 'Dark' : 'Light'}
+                                </p>
+                            </div>
+                        </button>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     );
 };
 
