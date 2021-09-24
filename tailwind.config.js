33a488f0c76dc43c3904b60999ef67f586de1b6e
kurniawanedi <edikurniawan27@outlook.com>
2021-09-24 21:43:33 +0700

    add font ubuntu

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                poppins: "'Poppins', 'sans-serif'",
+                ubuntu: "'Ubuntu', 'sans-serif'",
                 hind: "'Hind', 'sans-serif'",
             },
             colors: {
