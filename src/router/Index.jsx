3dc1bd021a5cf91ddac2f4d328fa3590ba222fce
kurniawanedi <edikurniawan27@outlook.com>
2021-09-25 19:10:03 +0700

    add link to resume page

--- src/router/Index.jsx
+++ src/router/Index.jsx
@@ -3,7 +3,9 @@ import { Switch, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import AboutMe from '../pages/About-Me';
 import Skill from '../pages/Skill';
+import Experience from '../pages/Experience';
 import Portfolio from '../pages/Portfolio';
+import Resume from '../pages/Resume';
 
 const Router = () => {
     return (
@@ -17,8 +19,14 @@ const Router = () => {
             <Route exact path="/skill">
                 <Skill />
             </Route>
+            <Route exact path="/experience">
+                <Experience />
+            </Route>
             <Route exact path="/portfolio">
                 <Portfolio />
+            </Route>
+            <Route exact path="/resume">
+                <Resume />
             </Route>
         </Switch>
     );
