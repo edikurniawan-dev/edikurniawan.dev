a8d24c92121d33e404ce99043f78fee74c377b5e
kurniawanedi <edikurniawan27@outlook.com>
2021-10-26 18:45:36 +0700

    save theme dark or light to localstorage

--- src/hooks/useDarkMode.jsx
+++ src/hooks/useDarkMode.jsx
@@ -1,15 +1,14 @@
-// import React from 'react';
 import { useState, useEffect } from 'react';
-// import { theme } from '../../tailwind.config';
 
 const useDarkMode = () => {
-    const [theme, setTheme] = useState('light');
+    const [theme, setTheme] = useState(localStorage.theme);
     const colorTheme = theme === 'light' ? 'dark' : 'light';
     useEffect(() => {
         const root = window.document.documentElement;
         root.classList.remove(colorTheme);
         root.classList.add(theme);
-    });
+        localStorage.setItem('theme', theme);
+    }, [theme, colorTheme]);
     return [colorTheme, setTheme];
 };
 
