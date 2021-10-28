b6e13b196d341b3261b6b8e86062796f9c6bd87d
kurniawanedi <edikurniawan27@outlook.com>
2021-10-28 12:23:58 +0700

    fix html class undefined in dark mode

--- src/hooks/useDarkMode.jsx
+++ src/hooks/useDarkMode.jsx
@@ -2,12 +2,20 @@ import { useState, useEffect } from 'react';
 
 const useDarkMode = () => {
     const [theme, setTheme] = useState(localStorage.theme);
-    const colorTheme = theme === 'light' ? 'dark' : 'light';
+
+    const colorTheme = theme === 'dark' ? 'light' : 'dark';
+
     useEffect(() => {
         const root = window.document.documentElement;
-        root.classList.remove(colorTheme);
-        root.classList.add(theme);
-        localStorage.setItem('theme', theme);
+        if (theme === undefined) {
+            root.classList.remove(undefined);
+            root.classList.add('light');
+            localStorage.setItem('theme', 'light');
+        } else {
+            root.classList.remove(colorTheme);
+            root.classList.add(theme);
+            localStorage.setItem('theme', theme);
+        }
     }, [theme, colorTheme]);
     return [colorTheme, setTheme];
 };
