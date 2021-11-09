126c1026530e0b9e434b7590112d887b3e5c5f96
kurniawanedi <edikurniawan27@outlook.com>
2021-11-09 20:26:48 +0700

    change link download resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -20,7 +20,7 @@ const Resume = () => {
             <div className="flex justify-center items-center">
                 <div className="flex justify-center md:justify-start items-center font-mono space-x-6 font-medium text-base md:text-lg pt-5">
                     <a
-                        href="https://doc-0s-b8-docs.googleusercontent.com/docs/securesc/e3fc7ttrv1mkn3annl749rd8vi6cl0t7/l4lkn2c5rcdgb2cm0rrht36h4ccdvaqe/1636463625000/11239491660057031462/11239491660057031462/1rKT5To63JC_uIF3xFvopNXm_zwOEDd2h?e=download&authuser=3&nonce=7qtbnvvlreuh2&user=11239491660057031462&hash=fel5186nc7hvdk61hod36bdfu4gv1mdv"
+                        href="https://drive.google.com/uc?id=1rKT5To63JC_uIF3xFvopNXm_zwOEDd2h&export=download"
                         className="text-white dark:text-dark bg-dark dark:bg-light py-2 px-4 md:px-8 border-2 border-dark dark:border-light hover:bg-light dark:hover:bg-dark hover:text-dark dark:hover:text-light">
                         <i className="fas fa-file-download"></i>
                         &nbsp; Resume
