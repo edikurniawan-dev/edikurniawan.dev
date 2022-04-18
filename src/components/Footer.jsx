a1de2d2df224153f4efdaa1259a1f1a571994bed
Edi Kurniawan <edikurniawan.dev@gmail.com>
2022-04-18 12:54:57 +0700

    [update] footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,17 +5,17 @@ const ExperienceCard = (props) => {
         <div className="flex items-center justify-center py-3 text-sm bg-black text-light">
             Created with &nbsp;
             <span>
-                <i className="fa-solid fa-heart"></i>
+                <i className="text-red-500 fa-solid fa-heart animate-pulse"></i>
             </span>
             &nbsp; and Inspired by &nbsp;
             <a
                 href="https://www.freecodecamp.org/"
-                className="hover:underline"
+                className="flex items-center font-mono underline"
                 target="_blank"
                 rel="noreferrer">
                 freeCodeCamp
                 <span>
-                    <i className="fa-brands fa-free-code-camp"></i>
+                    <i className="text-lg fa-brands fa-free-code-camp"></i>
                 </span>
             </a>
         </div>
