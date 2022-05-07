d6d27c522c8be6014f7972e26d2e5e9c0f3ac179
Edi Kurniawan <edikurniawan.dev@gmail.com>
2022-05-07 16:04:48 +0700

    add 404 page

---
+++ pages/404.jsx
@@ -0,0 +1,16 @@
+import Layout from '../components/layout';
+
+export default function Custom404() {
+    return (
+        <Layout>
+            <div className="flex flex-col items-center justify-center h-[70vh] pt-5 md:pt-10">
+                <span className="font-bold text-7xl md:text-9xl text-gray">
+                    404
+                </span>
+                <span className="text-base font-bold md:text-2xl text-dark dark:text-light">
+                    Page not found.
+                </span>
+            </div>
+        </Layout>
+    );
+}
