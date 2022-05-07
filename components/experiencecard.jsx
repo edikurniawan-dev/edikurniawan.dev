a71668d6228312150b7a68966cac249fd10912c4
Edi Kurniawan <edikurniawan.dev@gmail.com>
2022-05-07 15:55:19 +0700

    add experience page

---
+++ components/experiencecard.jsx
@@ -0,0 +1,32 @@
+export default function ExperienceCard({
+    place,
+    padding,
+    companyName,
+    during,
+    task,
+    number,
+}) {
+    return (
+        <div className="flex flex-col items-center lg:flex-row">
+            <div className={place}>
+                <div className={padding}>
+                    <div className="p-6 bg-white dark:bg-light">
+                        <h3 className="font-mono text-xl font-bold text-dark">
+                            {companyName}
+                        </h3>
+                        <p className="mb-2 font-mono text-lg font-bold text-dark">
+                            {during}
+                        </p>
+                        <div className="border border-dark bg-dark"></div>
+                        <p className="mt-3 font-sans tracking-wider text-dark">
+                            {task}
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="absolute flex items-center justify-center text-lg font-bold text-white transform -translate-x-1/2 -translate-y-6 border-2 bg-dark dark:bg-light dark:text-dark w-9 h-9 left-1/2 lg:translate-y-0 border-dark">
+                {number}
+            </div>
+        </div>
+    );
+}
--- pages/experience.jsx
+++ pages/experience.jsx
@@ -1,11 +1,49 @@
 import Layout from '../components/layout';
+import ExperienceCard from '../components/experiencecard';
 
 export default function Experience() {
     return (
         <Layout>
-            <div className="mx-auto md:container md:px-8 md:max-w-6xl 2xl:max-w-7xl font-mono">
-                <div className="pt-5 md:pt-10">Experience</div>
-            </div>
+            <main className="flex flex-col pt-5 md:pt-10">
+                <h1 className="font-mono text-2xl font-bold underline md:text-3xl text-dark dark:text-light">
+                    Experience
+                </h1>
+                <br />
+
+                <div className="flex flex-col justify-center h-full -mt-10 md:-mt-0">
+                    <div className="w-full px-0 py-0 lg:py-3 sm:mx-auto">
+                        <div className="relative space-y-16 text-gray-700 lg:space-y-0">
+                            {/* line vertical */}
+                            <div className="absolute hidden w-1 h-full transform -translate-x-1/2 opacity-50 lg:block bg-dark dark:bg-light left-1/2"></div>
+
+                            <ExperienceCard
+                                place="flex justify-start w-full mx-auto items-center"
+                                padding="w-full lg:w-1/2 lg:pr-20"
+                                companyName="PT Banopolis Inovasi Kendara (Intern)"
+                                during="Front-End Web Developer | June 2020 - July 2020"
+                                task="Develop UI website dashboard admin of Jaramba apps. I develope using figma, bootstrap, and sweetalert. This application serves to monitor the location of the bus that is running. In addition to storing route data, drivers, buses and bus travel history."
+                                number="1"
+                            />
+                            <ExperienceCard
+                                place="flex justify-end w-full mx-auto items-center"
+                                padding="w-full lg:w-1/2 lg:pl-20"
+                                companyName="Freeleance"
+                                during="Web Developer | June 2020 - Present"
+                                task="Develop a website according to the client's request."
+                                number="2"
+                            />
+                            <ExperienceCard
+                                place="flex justify-start w-full mx-auto items-center"
+                                padding="w-full lg:w-1/2 lg:pr-20"
+                                companyName="CV Newus Teknologi"
+                                during="Full-Stack Web Developer | November 2021 - March 2022"
+                                task="Develop websites both frontend and backend according to client needs."
+                                number="3"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </main>
         </Layout>
     );
 }
