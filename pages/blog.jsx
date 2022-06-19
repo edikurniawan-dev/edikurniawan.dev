1653ba8d889e3457299307e5781baf8bbaa14ac4
Edi Kurniawan <edikurniawan.dev@gmail.com>
2022-06-19 19:47:48 +0700

    update blog

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -1,4 +1,7 @@
 import Layout from '../components/layout';
+import Image from 'next/image';
+import ImageLoader from '../src/ImageLoader';
+import MyPhoto from '../images/under-construction.svg';
 
 export default function Blog() {
     return (
@@ -9,8 +12,19 @@ export default function Blog() {
             <br />
 
             {/* <div className="flex flex-col justify-center h-full -mt-10 md:-mt-0"></div> */}
-            <div className="flex justify-center font-mono text-4xl font-bold dark:text-light">
-                soon..
+            <div className="flex flex-col items-center mt-16">
+                <div className="w-48 md:w-96">
+                    <Image
+                        loader={ImageLoader}
+                        alt="Edi Kurniawan"
+                        src={MyPhoto}
+                        unoptimized
+                        priority
+                    />
+                </div>
+                <span className="mt-5 font-mono text-xl font-medium md:text-2xl md:mt-10 dark:text-light">
+                    Under Construction
+                </span>
             </div>
         </Layout>
     );
