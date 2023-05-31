9f63a52cd6d4353f87a2cdf2a7704b3514ce9306
Edi Kurniawan <edikurniawan.dev@gmail.com>
2023-05-31 09:38:40 +0700

    add vercel analitycs

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '@fortawesome/fontawesome-svg-core/styles.css';
+import { Analytics } from '@vercel/analytics/react';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import { ThemeProvider } from 'next-themes';
 import '../styles/globals.css';
@@ -6,9 +7,12 @@ config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
     return (
-        <ThemeProvider enableSystem={true} attribute="class">
-            <Component {...pageProps} />
-        </ThemeProvider>
+        <>
+            <ThemeProvider enableSystem={true} attribute="class">
+                <Component {...pageProps} />
+            </ThemeProvider>
+            <Analytics />
+        </>
     );
 }
 
