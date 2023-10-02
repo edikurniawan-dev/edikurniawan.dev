47d11d744d52869a1ac1788ca7638a31cc417517
Edi Kurniawan <edikurniawan.dev@gmail.com>
2023-10-02 15:08:01 +0700

    chore: :wrench: configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb', 'next/core-web-vitals'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'import/no-extraneous-dependencies': 'off',
+  },
+};
