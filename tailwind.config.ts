528373bdcfcb6526e2b2f6609d075498b2632da8
Edi Kurniawan <edikurniawan.dev@gmail.com>
2024-05-14 02:29:30 +0700

    chore: :art: add animation carousel

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,23 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      // backgroundImage: {
+      //   "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      //   "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      // },
+      animation: {
+        "loop-scroll": "loop-scroll 120s linear infinite",
+        "loop-scroll2": "loop-scroll2 120s linear infinite",
+      },
+      keyframes: {
+        "loop-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
+        "loop-scroll2": {
+          from: { transform: "translateX(-100%)" },
+          to: { transform: "translateX(0)" },
+        },
       },
     },
   },
