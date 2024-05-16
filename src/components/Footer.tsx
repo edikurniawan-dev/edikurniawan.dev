83b214e827209c2c399498c09e24afe0c9124e85
Edi Kurniawan <edikurniawan.dev@gmail.com>
2024-05-17 00:55:51 +0700

    refactor: :fire: add inside button

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,24 @@
 import Link from "next/link";
 import React from "react";
 import { SiGithub, SiLinkedin } from "react-icons/si";
+import { Button } from "./ui/button";
 
 export function Footer() {
   return (
     <footer className="h-16 border-t border-dashed px-10">
       <div className="flex h-full w-full items-center justify-between border-x border-dashed px-10">
         <div className="text-xs">© 2024 edikurniawan.dev by Edi Kurniawan. All rights reserved.</div>
-        <div className="flex items-center gap-2.5">
-          <Link href="https://www.linkedin.com/in/edikurniawan-dev/" target="_blank">
-            <SiLinkedin className="size-5" />
-          </Link>
-          <Link href="https://github.com/edikurniawan-dev" target="_blank">
-            <SiGithub className="size-5" />
-          </Link>
+        <div className="flex items-center">
+          <Button size="icon" variant="ghost">
+            <Link href="https://www.linkedin.com/in/edikurniawan-dev/" target="_blank">
+              <SiLinkedin className="size-5" />
+            </Link>
+          </Button>
+          <Button size="icon" variant="ghost">
+            <Link href="https://github.com/edikurniawan-dev" target="_blank">
+              <SiGithub className="size-5" />
+            </Link>
+          </Button>
         </div>
       </div>
     </footer>
