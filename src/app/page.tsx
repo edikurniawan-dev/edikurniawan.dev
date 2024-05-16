af8f343ce7b5bb6eabae2f7b86f15f72b84c99ae
Edi Kurniawan <edikurniawan.dev@gmail.com>
2024-05-17 00:55:29 +0700

    refactor: :fire: change using badge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Carousel from "./components/Carousel";
 import { SiLaravel, SiNextdotjs, SiNodedotjs, SiReact } from "react-icons/si";
 import Link from "next/link";
 import Tag from "./components/Tag";
+import { Badge } from "@/components/ui/badge";
 
 export default function Home() {
   return (
@@ -14,10 +15,34 @@ export default function Home() {
           <div className="mt-4 leading-relaxed">
             I'm <b>Edi Kurniawan</b> a Web Developer with work experience in web application development. I have skills
             in developing web applications using several technologies like{" "}
-            <Tag title="ReactJS" icon={<SiReact />} href="https://react.dev/" /> with{" "}
-            <Tag title="NextJS" icon={<SiNextdotjs />} href="https://nextjs.org/" /> as react framework in frontend and
-            <Tag title="Laravel" icon={<SiLaravel />} href="https://laravel.com/" /> or{" "}
-            <Tag title="NodeJS" icon={<SiNodedotjs />} href="https://nodejs.org/" /> in backend.
+            <Badge variant="outline">
+              <Link href="https://react.dev/" target="_blank" className="flex items-center gap-1">
+                <SiReact />
+                ReactJS
+              </Link>
+            </Badge>{" "}
+            with{" "}
+            <Badge variant="outline" className="gap-1">
+              <Link href="https://nextjs.org/" target="_blank" className="flex items-center gap-1">
+                <SiNextdotjs />
+                NextJS
+              </Link>
+            </Badge>{" "}
+            as react framework in frontend and
+            <Badge variant="outline" className="gap-1">
+              <Link href="https://laravel.com/" target="_blank" className="flex items-center gap-1">
+                <SiLaravel />
+                Laravel{" "}
+              </Link>
+            </Badge>{" "}
+            or{" "}
+            <Badge variant="outline" className="gap-1">
+              <Link href="https://nodejs.org/" target="_blank" className="flex items-center gap-1">
+                <SiNodedotjs />
+                NodeJS
+              </Link>
+            </Badge>{" "}
+            in backend.
           </div>
         </div>
       </section>
