84c5b722eced83d3f7ab4d5c44ff7ef4b78ba8ff
Edi Kurniawan <edikurniawan.dev@gmail.com>
2024-05-24 00:53:51 +0700

    feat: :sparkles: add analytics and speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Source_Sans_3 } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
@@ -25,6 +27,8 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
             </div>
           </div>
         </ThemeProvider>
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
