1963c04db1182ab17242b1b988724fdf2c060e67
Edi Kurniawan <edikurniawan.dev@gmail.com>
2024-05-25 15:47:24 +0700

    feat: :sparkles: add website and linkedin url

---
+++ src/app/experiences/components/option-toggle.tsx
@@ -0,0 +1,43 @@
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Link from "next/link";
+import { FaEllipsisVertical, FaLinkedin } from "react-icons/fa6";
+import { TbWorldUpload } from "react-icons/tb";
+
+interface OptionToggleProps {
+  companyWebsite: string | null;
+  companyLinkedin: string | null;
+}
+
+export function OptionToggle({ companyWebsite, companyLinkedin }: OptionToggleProps) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button size="icon" variant="secondary" className="h-7 w-7 border border-dashed">
+          <FaEllipsisVertical className="size-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        {companyWebsite && (
+          <Link href={companyWebsite} target="_blank">
+            <DropdownMenuItem className="flex cursor-pointer gap-2">
+              <TbWorldUpload className="size-5" /> Website
+            </DropdownMenuItem>
+          </Link>
+        )}
+        {companyLinkedin && (
+          <Link href={companyLinkedin} target="_blank">
+            <DropdownMenuItem className="flex cursor-pointer gap-2">
+              <FaLinkedin className="size-5" /> LinkedIn
+            </DropdownMenuItem>
+          </Link>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
